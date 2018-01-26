8fc1d9801e9020b8632d4e9bad8a2346998ebbfb
Jaiana Dixon <jdixon@wlapcs.org>
2018-01-26 01:58:04 +0000

    done

--- sketch.js
+++ sketch.js
@@ -2,15 +2,43 @@ var lpkidsred;
 var lpkidsgreen;
 var lpkidsblue;
 var lpkidsgray;
+var oldTV;
 
 function preload(){
-    
+    lpkidsgray=loadImage("images/lpkids-gray.png");
+    lpkidsred=loadImage("images/lpkids-red.png");
+    lpkidsgreen=loadImage("images/lpkids-green.png");
+    lpkidsblue=loadImage("images/lpkids-blue.png");
+    oldTV=loadImage("images/oldTV.png");
 }
-
+ 
 function setup(){
-    createCanvas(500,374);
+    createCanvas(749,524).parent("sketch-folder");
 }
 
 function draw(){
-    
+    if(keyIsPressed){
+        if(key=="r"){
+            image(lpkidsred, 50, 50);
+        }
+        if(key=="g"){
+            image(lpkidsgreen, 50, 50);
+        }
+        if(key=="b"){
+            image(lpkidsblue ,50, 50);
+        }
+        if(keyCode==32){
+            fill(0);
+            rect(50,50,500,374);
+            blendMode(LIGHTEST);
+            image(lpkidsred,50,50);
+            image(lpkidsgreen,50,50);
+            image(lpkidsblue,50,50);
+        }
+    }
+    else{
+        image(lpkidsgray , 50, 50);
+    }
+    blendMode(NORMAL);
+    image(oldTV,0,0);
 }
